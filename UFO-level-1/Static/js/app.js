708a1ddf29e85a3db5cc93c96b6a8d24930b7267
masontotall <68935404+masontotall@users.noreply.github.com>
2021-06-05 16:44:04 -0500

    finished the app.js so it would remove the original table and display filtered data when clicking the filter button. Page refreshes after filtering and the button only works when clicked. Works as intended though.

--- UFO-level-1/Static/js/app.js
+++ UFO-level-1/Static/js/app.js
@@ -1,8 +1,12 @@
 // from data.js
+
 var tableData = data;
 
+
 // YOUR CODE HERE!
+
 var tbody =d3.select("tbody");
+
 
 // Read and display entire dataset from data.js
 
@@ -16,3 +20,43 @@ tableData.forEach((report) => {
         cell.text(value);
     });
 });
+
+
+// Specify the filter button
+
+var filter = d3.select("#filter-btn");
+
+
+//create a function that runs and filters by date when the filter button is clicked
+
+filter.on("click", function() {
+    
+    d3.select("tbody").html("");
+    
+
+    // read the input
+
+    var dateTime = d3.select("#datetime").property("value");
+    console.log(dateTime);
+
+    
+    // Filter the data set for the input
+
+    var filteredData = tableData.filter(record => record.datetime === dateTime);
+    console.log(filteredData);
+
+    
+    // Display Results
+
+    filteredData.forEach((report) => {
+        var row = tbody.append('tr');
+    
+        Object.entries(report).forEach(([key, value]) => {
+            console.log(key, value);
+
+            var cell = row.append('td');
+            cell.text(value);
+        });
+    });
+    
+    });
